bb4c9c3c0048cdb8215d7aaff9612f3be7be051b
Blake Seigler <blake_seigler@unc.edu>
2024-08-14 23:37:57 -0400

    added random stuff here and there

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,17 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
-app.get("/api", )
+
+// Serve the static files from the React app's build directory
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Handle any requests that don't match static files, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
